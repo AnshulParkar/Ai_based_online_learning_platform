83c04450490cfc1711a57ea5d0d21f8d9e14e198
AnshulParkar <121174809+AnshulParkar@users.noreply.github.com>
2025-03-17 15:09:59 +0530

    footer

--- lms/app/page.tsx
+++ lms/app/page.tsx
@@ -4,11 +4,13 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Header from "@/components/header";
 import { Hero } from "@/components/herosection";
+import { Footerdemo } from "@/components/ui/footer-section";
 export default function Home() {
   return (
     <div>
       <Header/>
       <Hero/>
+      <Footerdemo />
     </div>
 
   );
